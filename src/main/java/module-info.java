9fc05c3f3073faa6a003b13e7a185a4f377c81b6
Claudia <clodos_m92@hotmail.com>
2021-12-16 16:43:56 +0100

    Inital Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+
+module com.example.producttabledb {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.example.producttabledb to javafx.fxml;
+    exports com.example.producttabledb;
+}
