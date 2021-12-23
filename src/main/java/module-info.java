09a6af2ccc71409d625e3272b2d65c69b3e324e8
Claudia <clodos_m92@hotmail.com>
2021-12-23 12:11:39 +0100

    Stylesheet ergänzt

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,4 @@
 
-/*module com.example.producttabledb {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.producttabledb to javafx.fxml;
-    exports com.example.producttabledb;
-
- /*
-
-
- */
 module com.example.producttabledb {
     requires javafx.controls;
     requires javafx.fxml;
